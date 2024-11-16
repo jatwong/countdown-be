a38df7b56f303c2dd73429990bce65f3e55ac3f0
Justina Wong <justinawong97@gmail.com>
2024-11-16 10:24:07 -0800

    update env variables

--- countdown/settings.py
+++ countdown/settings.py
@@ -10,20 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+from os import path
+from environ import Env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Initialize the environ object
+env = Env()
 
+
+BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+ENV_FILE = f"{BASE_DIR}/.env"
+
+if path.exists(ENV_FILE):
+    env.read_env(ENV_FILE)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env.str("SECRET_KEY", default="not-so-secret")
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
