ed426208c8c3443186487f92c4069ff0434fee48
Justina Wong <justinawong97@gmail.com>
2024-11-16 10:46:20 -0800

    add database connection

--- countdown/asgi.py
+++ countdown/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'countdown.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "countdown.settings")
 
 application = get_asgi_application()
