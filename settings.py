78f5c2c5380b6374c365a19ad3b38ecec8f48b29
Justina Wong <justinawong97@gmail.com>
2024-11-16 12:52:05 -0800

    create user app, reorganize

--- settings.py
+++ settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "countdown.api.apps",
+    "countdown.apps.CountdownConfig",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
--- urls.py
+++ urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("countdown.api.urls")),
+    path("api/", include("countdown.urls")),
 ]
